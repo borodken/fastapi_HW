ebb53cb288a78cd855c028c5c9d3afd4a98a4fd0
borodken <mihail.borodkin2706@gmail.com>
2025-08-16 17:22:41 +0400

    add OAuth2

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, status, HTTPException
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from sqlalchemy import select, insert
 
 from app.models.user import User
@@ -7,32 +6,46 @@ from app.schemas import CreateUser
 from app.backend.db_depends import get_db
 from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from passlib.context import CryptContext
+
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
-security = HTTPBasic()
 
+async def authenticate_user(db: Annotated[AsyncSession, Depends(get_db)], username: str, password: str):
+    user = await db.scalar(select(User).where(User.username == username))
 
-async def get_current_username(db: Annotated[AsyncSession, Depends(get_db)],
-                               credentials: HTTPBasicCredentials = Depends(security)):
-    user = await db.scalar(select(User).where(User.username == credentials.username))
-
-    if not user or not bcrypt_context.verify(credentials.password, user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    if not user or not bcrypt_context.verify(password, user.hashed_password) or user.is_active is False:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
     return user
 
 
-@router.get("/users/me")
-async def read_current_user(user: str = Depends(get_current_username)):
-    return {"User": user}
+@router.post("/token")
+async def login(db: Annotated[AsyncSession, Depends(get_db)], form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = await authenticate_user(db, form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate user"
+        )
+
+    return {
+        "access_token": user.username,
+        "token_type": "bearer"
+    }
 
 
 @router.post("/")
-async def create_user(create_user: CreateUser, db: Annotated[AsyncSession, Depends(get_db)]):
+async def create_user(db: Annotated[AsyncSession, Depends(get_db)], create_user: CreateUser):
     await db.execute(insert(User).values(
         first_name=create_user.first_name,
         last_name=create_user.last_name,
@@ -48,3 +61,6 @@ async def create_user(create_user: CreateUser, db: Annotated[AsyncSession, Depen
         "transaction": "Successful"
     }
 
+@router.get("/read_current_user")
+async def read_current_user(user: Annotated[User, Depends(oauth2_scheme)]):
+    return user
